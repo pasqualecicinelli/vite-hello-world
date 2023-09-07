c1826050a766425672f083c31a2884f8e27ac1d1
pasqualecicinelli <pasquale.cicinelli89@gmail.com>
2023-09-07 17:06:11 +0200

    feat AppTitle

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,35 @@
 <script>
-export default {
-    data() {
-        return {
-            title:'Ciao'
-        }
-    },
-}
+import AppTitle from "./components/AppTitle.vue";
 
+export default {
+  data() {
+    return {
+      title: "Ecco",
+    };
+  },
+
+  components: { AppTitle },
+};
 </script>
 
-
 <template>
+  <div class="container">
+    <h1>{{ title }}</h1>
 
-<h1>{{ title }}</h1>
+    <AppTitle></AppTitle>
 
+  </div>
 </template>
 
-
-<style>
-
-h1{
-    color: red;
+<style s>
+.container{
+    margin-left:auto;
+    margin-right: auto;
+    width: 1000px;
+    text-align: center;
 }
 
+h1 {
+  color: red;
+}
 </style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    data() {
+        return {
+            titleApp: 'La mia prima app con Vite!',
+        }
+    },
+};
+
+
+</script>
+
+<template>
+
+<header>
+
+<h1>{{ titleApp }}</h1>
+
+</header>
+
+</template>
+
+
+<style scoped>
+
+h1{
+    color: blue;
+}
+
+</style>

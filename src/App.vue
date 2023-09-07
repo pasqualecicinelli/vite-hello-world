c97a249536e61186fb23f9e3918a16c2e3c01921
pasqualecicinelli <pasquale.cicinelli89@gmail.com>
2023-09-07 16:27:23 +0200

    feat vue

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    data() {
+        return {
+            title:'Ciao'
+        }
+    },
+}
+
+</script>
+
+
+<template>
+
+<h1>{{ title }}</h1>
+
+</template>
+
+
+<style>
+
+h1{
+    color: red;
+}
+
+</style>
